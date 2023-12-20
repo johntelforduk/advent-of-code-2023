68f46a785fe3a8305af9f64fe27a19c0e74c1ad9
johntelforduk <jtelford1@gmail.com>
2023-12-20 13:02:16 +0000

    Solution to day 16, part 2.

--- 16-the-floor-will-be-lava/solution.py
+++ 16-the-floor-will-be-lava/solution.py
@@ -3,6 +3,7 @@
 
 from icecream import ic
 
+
 class Beam:
 
     def __init__(self, layout: dict, x:int, y:int, direction: str):
@@ -13,15 +14,6 @@ class Beam:
 
         self.max_x, self.max_y = max(self.layout)
 
-    def render(self):
-        print()
-        for y in range(self.max_x + 1):
-            for x in range(self.max_y + 1):
-                if (x, y) in energised:
-                    print('#', end='')
-                else:
-                    print('.', end='')
-            print()
 
     def move(self) -> str:
         deltas = {'L': (-1, 0), 'R': (1, 0), 'U': (0, -1), 'D': (0, 1)}
@@ -58,6 +50,41 @@ class Beam:
         return ''
 
 
+def render(beam: Beam, energised: set):
+    for y in range(beam.max_x + 1):
+        for x in range(beam.max_y + 1):
+            if (x, y) in energised:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+
+def test_configuration(layout: dict, x: int, y: int, direction: str):
+    first_beam = Beam(layout=layout, x=x, y=y, direction=direction)
+    first_beam.move()
+    beams = [first_beam]
+    energised_direction = set()
+    energised = set()
+
+    while len(beams) > 0:
+        this_beam = beams.pop(0)
+
+        while ((this_beam.x, this_beam.y, this_beam.direction) not in energised_direction
+               and 0 <= this_beam.x <= this_beam.max_x
+               and 0 <= this_beam.y <= this_beam.max_y):
+            energised_direction.add((this_beam.x, this_beam.y, this_beam.direction))
+            energised.add((this_beam.x, this_beam.y))
+            new_direction = this_beam.move()
+
+            if new_direction != '':
+                beams.append(Beam(layout=layout, x=this_beam.x, y=this_beam.y, direction=new_direction))
+
+    beam = Beam(layout=layout, x=3, y=-1, direction='D')
+    # render(beam, energised)
+    return len(energised)
+
+
 with open('input.txt', 'r') as file:
     contraption = file.read()
 
@@ -72,27 +99,16 @@ for line in contraption.split('\n'):
         x += 1
     y += 1
 
-first_beam = Beam(layout=layout, x=-1, y=0, direction='R')
-first_beam.move()
-beams = [first_beam]
-energised_direction = set()
-energised = set()
+ic(test_configuration(layout=layout, x=-1, y=0, direction='R'))
 
-while len(beams) > 0:
-    this_beam = beams.pop(0)
-    ic(len(beams))
+mx, my = max(layout)
+assert mx == my
+best = 0
 
-    while ((this_beam.x, this_beam.y, this_beam.direction) not in energised_direction
-           and 0 <= this_beam.x <= this_beam.max_x
-           and 0 <= this_beam.y <= this_beam.max_y):
-        energised_direction.add((this_beam.x, this_beam.y, this_beam.direction))
-        energised.add((this_beam.x, this_beam.y))
-        new_direction = this_beam.move()
+for i in range(mx + 1):
+    best = max(best, test_configuration(layout=layout, x=-1, y=i, direction='R'))
+    best = max(best, test_configuration(layout=layout, x=i, y=-1, direction='D'))
+    best = max(best, test_configuration(layout=layout, x=mx + 1, y=i, direction='L'))
+    best = max(best, test_configuration(layout=layout, x=i, y=my + 1, direction='U'))
 
-        if new_direction != '':
-            beams.append(Beam(layout=layout, x=this_beam.x, y=this_beam.y, direction=new_direction))
-            ic(len(beams))
-
-beam = Beam(layout=layout, x=0, y=0, direction='R')
-beam.render()
-ic(len(energised))
+ic(best)
